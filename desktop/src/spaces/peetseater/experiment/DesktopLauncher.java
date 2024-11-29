f58fc2c6e74942621623c8b9ee08c5fd47f7041e
Ethan Eldridge <ejayeldridge@gmail.com>
2024-11-29 09:48:38 -0500

    Initial Project
    sans gradle jars because you dont need those, also a gitignore file
    because gdx doesn't supply that.

---
+++ desktop/src/spaces/peetseater/experiment/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package spaces.peetseater.experiment;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import spaces.peetseater.experiment.PrototypeMemoryExperiment;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PrototypeMemoryExperiment");
+		new Lwjgl3Application(new PrototypeMemoryExperiment(), config);
+	}
+}
---
+++ html/src/spaces/peetseater/experiment/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package spaces.peetseater.experiment.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import spaces.peetseater.experiment.PrototypeMemoryExperiment;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PrototypeMemoryExperiment();
+        }
+}
